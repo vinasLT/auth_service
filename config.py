6fb664d010ac3d3c6944fe24f443bcacb50e929c
peyro <peyrovskaaa@gmail.com>
2025-08-12 12:04:26 +0200

    add github workflow

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ class Settings(BaseSettings):
 
     # AWS
     AWS_REGION: str = "eu-north-1"
-    AWS_ACCESS_KEY_ID: str
-    AWS_SECRET_ACCESS_KEY: str
+    AWS_ACCESS_KEY_ID: str = 'test-key'
+    AWS_SECRET_ACCESS_KEY: str = 'test-key'
     AWS_KMS_KEY_ARN: str = 'arn:aws:kms:eu-north-1:533266961080:key/71dc53ed-5b1d-43fe-b8ca-5a4ba731cc90'
 
     # Security
