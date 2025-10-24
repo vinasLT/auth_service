596892ccaa02b9cb07a21f3901b0390eac237091
ivanpetrovskij <peyrovskaaa@gmai.com>
2025-10-24 22:14:45 +0200

    add phone_number to access token_payload

--- routers/v1/public/verify.py
+++ routers/v1/public/verify.py
@@ -7,13 +7,13 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from core.logger import logger
 from database.crud.user import UserService
 from database.db.session import get_async_db
-from dependencies.security import get_current_user
+from dependencies.security import get_current_user, JWTUser
 
 verify_request_router = APIRouter()
 
 @verify_request_router.api_route("/verify", methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
                                  operation_id="verify_auth_request")
-async def verify_request(request: Request, payload: dict = Security(get_current_user),
+async def verify_request(request: Request, payload: JWTUser = Security(get_current_user),
                          db: AsyncSession = Depends(get_async_db)):
     try:
         original_method = request.headers.get("X-Forwarded-Method", "GET")
@@ -21,8 +21,8 @@ async def verify_request(request: Request, payload: dict = Security(get_current_
         original_uri = request.headers.get("X-Forwarded-Uri", "")
 
         logger.info(f"Verifying user token", extra={
-            "user_id": payload.get('sub'),
-            "email": payload.get('email'),
+            "user_id": payload.id,
+            "email": payload.email,
             "original_method": original_method,
             "original_host": original_host,
             "original_uri": original_uri
@@ -30,10 +30,12 @@ async def verify_request(request: Request, payload: dict = Security(get_current_
 
         user_service = UserService(db)
 
-        user = await user_service.get_user_by_uuid(str(payload.get("sub")))
+        print(payload.id)
+
+        user = await user_service.get_user_by_uuid(str(payload.id))
         if not user:
-            logger.warning(f'Authentication failed - user not found', extra={'user_uuid': payload.get('sub'),
-                                                                             'jti': payload.get('jti')})
+            logger.warning(f'Authentication failed - user not found', extra={'user_uuid': payload.id,
+                                                                             'jti': payload.token_jti})
             raise UnauthorisedProblem("User not found")
         roles_permissions = await user_service.extract_roles_and_permissions_from_user(user_id=user.id, user=user)
 
