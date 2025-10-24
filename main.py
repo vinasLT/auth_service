10f75849c75c8e4a3d45ddacebc80b631a1e3b34
ivanpetrovskij <peyrovskaaa@gmai.com>
2025-10-24 21:06:33 +0200

    add phone_number to access token_payload

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = create_app()
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, port=8000)
+    uvicorn.run(app, port=8001)
--- routers/v1/public/verify.py
+++ routers/v1/public/verify.py
@@ -1,15 +1,20 @@
 from fastapi import APIRouter, Security, Request
+from fastapi.params import Depends
 from fastapi.responses import JSONResponse
 from rfc9457 import UnauthorisedProblem
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.logger import logger
+from database.crud.user import UserService
+from database.db.session import get_async_db
 from dependencies.security import get_current_user
 
 verify_request_router = APIRouter()
 
 @verify_request_router.api_route("/verify", methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
                                  operation_id="verify_auth_request")
-async def verify_request(request: Request, payload: dict = Security(get_current_user)):
+async def verify_request(request: Request, payload: dict = Security(get_current_user),
+                         db: AsyncSession = Depends(get_async_db)):
     try:
         original_method = request.headers.get("X-Forwarded-Method", "GET")
         original_host = request.headers.get("X-Forwarded-Host", "")
@@ -23,17 +28,21 @@ async def verify_request(request: Request, payload: dict = Security(get_current_
             "original_uri": original_uri
         })
 
+        user_service = UserService(db)
+
+        user = await user_service.get_user_by_uuid(str(payload.get("sub")))
+        roles_permissions = await user_service.extract_roles_and_permissions_from_user(user_id=user.id, user=user)
 
 
-        roles = ",".join(payload.get("roles", []))
-        permissions = ",".join(payload.get("permissions", []))
+        roles = ",".join(roles_permissions.get("roles", []))
+        permissions = ",".join(roles_permissions.get("permissions", []))
 
         response_headers = {
             "X-User-ID": str(payload.get('sub')),
             "X-User-Email": payload.get("email", ""),
             "X-User-Role": roles,
             "X-Token-Expires": str(payload.get("exp", "")),
-            "X-Permissions": permissions,
+            "X-Permissions": permissions
         }
 
         logger.info(f"Authentication successful for user: {payload.get('sub')}")
